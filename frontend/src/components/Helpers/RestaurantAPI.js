4dfe3c5ff551b347ebaaeeb5a50debc06adf7b98
Rodrigo dos Santos <rodrigo.contato@live.com>
2021-08-10 19:19:41 -0300

    PageLogin_3

--- frontend/src/components/Helpers/RestaurantAPI.js
+++ frontend/src/components/Helpers/RestaurantAPI.js
@@ -1,9 +1,58 @@
+import Cookies from "js-cookie"
+import qs from 'qs'
+
+const BASEAPI = 'http://localhost:5000'
+
+const apiFetchPost = async (endpoint, body) => {
+    if(!body.token){
+        let token = Cookies.get('token')
+        if(token){
+            body.token = token
+        }
+    }
+    const res = await fetch(BASEAPI+endpoint, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(body)
+    })
+    const json = await res.json()
+
+    if(json.notallowed){
+        window.location.href = '/login'
+    }
+
+    return json
+}
+
+//Quando a requisição for feita por query string
+const apiFetchGet = async (endpoint, body = []) => {
+    if(!body.token){
+        let token = Cookies.get('token')
+        if(token){
+            body.token = token
+        }
+    }
+    //recebe o objeto e transforma em query string
+    const res = await fetch(`${BASEAPI+endpoint}?${qs.stringify(body)}`)
+    const json = await res.json()
+
+    if(json.notallowed){
+        window.location.href = '/login'
+    }
+
+    return json
+}
 
 const RestautantAPI = {
 
     login: async (email, password) => {
-
-        return {error:'Funcionalidade Incompleta'}
+        const json = await apiFetchPost(
+            '/user/login', {email, password}
+        )
+        return json
     }
 }
 
